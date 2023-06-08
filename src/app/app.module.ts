edcd0a9409217912bf33727383cad81d7294ac2c
k.i <inokuchi.kazuyuki@inogucci-studio.info>
2023-06-08 14:04:51 +0900

    追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
